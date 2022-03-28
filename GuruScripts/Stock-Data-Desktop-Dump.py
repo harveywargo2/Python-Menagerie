553f1202c85239a77f60cd993f2cd7aa99faec53
Harvey Wargo <harveywargo.dev@gmail.com>
2022-03-28 16:57:25 -0400

    Create Stock-Data-Desktop-Dump.py

---
+++ GuruScripts/Stock-Data-Desktop-Dump.py
@@ -0,0 +1,12 @@
+import keys.guruapi
+import GuruWrangler.dividend
+import pandas as pd
+import os
+
+token = keys.guruapi.token
+ticker = input('Enter Stock Ticker: ').upper()
+
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+print(desktop)
+
