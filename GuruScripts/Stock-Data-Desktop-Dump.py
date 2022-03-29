059898ee201f6e1047b4dfcce666d3dcf6bdc924
Harvey Wargo <harveywargo.dev@gmail.com>
2022-03-29 17:40:39 -0400

    Update Stock-Data-Desktop-Dump.py

--- GuruScripts/Stock-Data-Desktop-Dump.py
+++ GuruScripts/Stock-Data-Desktop-Dump.py
@@ -1,13 +1,19 @@
 import keys.guruapi
-import GuruWrangler.dividend
-import pandas as pd
+import GuruWrangler.jupyterdata
+import GuruWrangler.dyt
 import os
 
 token = keys.guruapi.token
 ticker = input('Enter Stock Ticker: ').upper()
 
+# Windows
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 
-print(desktop)
+os.chdir(desktop)
+
+# GuruWrangler.jupyterdata.to_csv(ticker, token)
+# GuruWrangler.dyt.to_csv(ticker, token)
+
+print(GuruWrangler.jupyterdata.to_df(ticker, token))
 
 
