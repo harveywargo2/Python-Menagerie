65f19e1b9722d964cab4e6b94aa04cffb6deb3c4
Harvey Wargo <harveywargo.dev@gmail.com>
2022-03-29 11:12:10 -0400

    Update margin.py

--- GuruWrangler/margin.py
+++ GuruWrangler/margin.py
@@ -12,11 +12,8 @@ def to_df(ticker: str, token: str):
     margin_df['OperatingProfit'] = ann_df['income_statement.Operating Income'].astype(float)
     margin_df['OPEX'] = ann_df['income_statement.Total Operating Expense'].astype(float)
     margin_df['NetProfit'] = ann_df['income_statement.Net Income'].astype(float)
-    margin_df['CashFromOperations'] = ann_df['cashflow_statement.Cash Flow from Operations'].astype(float)
-    margin_df['CFOM'] = margin_df['CashFromOperations'] / margin_df['Revenue']
+    margin_df['CashFromOps'] = ann_df['cashflow_statement.Cash Flow from Operations'].astype(float)
     margin_df['CAPEX'] = ann_df['cashflow_statement.Capital Expenditure'].astype(float)
-    margin_df['CapexMargin'] = margin_df['CAPEX'] / margin_df['Revenue']
-    margin_df['FCFM'] = margin_df['FreeCashFlow'] / margin_df['Revenue']
     margin_df['FreeCashFlow'] = ann_df['cashflow_statement.Free Cash Flow'].astype(float)
     return margin_df
 
@@ -24,3 +21,4 @@ def to_df(ticker: str, token: str):
 def to_csv(ticker: str, token: str):
     margin_df = to_df(ticker, token)
     return margin_df.to_csv(ticker.upper() + '-MarginsData@' + str(date.today()) + '.csv')
+
