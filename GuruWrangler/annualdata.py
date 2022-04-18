7afc62c73bfc8ae44fc7a067e5ffcee72d45352d
Harvey Wargo <harveywargo.dev@gmail.com>
2022-04-18 09:32:47 -0400

    Update annualdata.py

--- GuruWrangler/annualdata.py
+++ GuruWrangler/annualdata.py
@@ -39,3 +39,4 @@ def to_df(ticker: str, token: str):
 def to_csv(ticker: str, token: str):
     data_df = to_df(ticker, token)
     return data_df.to_csv(ticker.upper() + '-AnnualData@' + str(date.today()) + '.csv')
+
